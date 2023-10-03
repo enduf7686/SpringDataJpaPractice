0d04bd5d7cfd3c521a276a2f9477a60973ab576d
enduf7686 <enduf768640@gmail.com>
2023-10-03 11:39:10 +0900

    예제 도메인 모델과 동작 확인

---
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,29 @@
+package study.datajpa.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    void testMember() {
+        Member member = new Member("memberA");
+        Member savedMember = memberRepository.save(member);
+
+        Member findMember = memberRepository.findById(member.getId()).orElse(null);
+        assertThat(savedMember.getId()).isEqualTo(findMember.getId());
+        assertThat(savedMember.getUsername()).isEqualTo(findMember.getUsername());
+        assertThat(savedMember).isEqualTo(member);
+    }
+}
